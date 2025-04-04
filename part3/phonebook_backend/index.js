e378e1913110b4790160646971f7f18a1799fc6d
martalj <96marta@gmail.com>
2025-04-04 16:43:50 +0200

    exercises 3.7-3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,15 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
 app.use(express.json())
+
+morgan.token('content', function getContent (request) {
+  return (JSON.stringify(request.body))
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     { 
--- tutorialpart3/index.js
+++ tutorialpart3/index.js
@@ -72,6 +72,21 @@ app.get('/', (request, response) => {
     response.json(note)
   })
   
+  const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+  
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+  app.use(requestLogger)
+  
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
